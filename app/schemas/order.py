46ad651dc3ec3b43d321e7cd310e39dc9c5b5423
ayugunin <ayugunin@sberautotech.ru>
2025-06-02 08:22:57 +0300

    fix: create_at only for OrderRead

--- app/schemas/order.py
+++ app/schemas/order.py
@@ -6,10 +6,10 @@ from app.schemas.benefit import BenefitInOrder
 class OrderBase(BaseModel):
     user_id: int
     snapshot: List[BenefitInOrder]
-    created_at: datetime
 
 class OrderCreate(OrderBase):
     pass
 
 class OrderRead(OrderBase, ORMBaseModel):
     id: int
+    created_at: datetime
